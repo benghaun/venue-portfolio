2fa1e54594c6ddf19235adf371f7e9e062042c9e
benghaun <ang.benghaun@gmail.com>
2018-10-11 13:42:22 +0300

    fixed another error with display

--- image_view/views.py
+++ image_view/views.py
@@ -6,15 +6,15 @@ import boto3
 def show_images(request):
     S3_BUCKET = os.environ.get('S3_BUCKET')
     s3 = boto3.client('s3', region_name='eu-west-3')
-    response = s3.list_objects(Bucket=S3_BUCKET).get('Contents')
-    image_filenames = [item['Key'] for item in response]
     urls = []
-    if image_filenames:
+    response = s3.list_objects(Bucket=S3_BUCKET).get('Contents')
+    if response:
+        image_filenames = [item['Key'] for item in response]
         for filename in image_filenames:
             url = s3.generate_presigned_url(ClientMethod="get_object",
                                             Params={'Bucket': S3_BUCKET,
                                                     'Key': filename},
                                             ExpiresIn=60)
-        urls.append(url)
+            urls.append(url)
     return render(request, 'image_view/image_view.html', {'urls': urls})
 
