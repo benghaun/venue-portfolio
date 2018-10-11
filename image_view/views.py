c2fe1214fcd42d3d7f4370f53b6f02bac58194f9
benghaun <ang.benghaun@gmail.com>
2018-10-11 13:39:08 +0300

    fixed issue with display page when bucket is empty

--- image_view/views.py
+++ image_view/views.py
@@ -1,22 +1,20 @@
 import os
-import json
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 import boto3
 
 
 def show_images(request):
     S3_BUCKET = os.environ.get('S3_BUCKET')
-
     s3 = boto3.client('s3', region_name='eu-west-3')
-    response = s3.list_objects(Bucket=S3_BUCKET)['Contents']
+    response = s3.list_objects(Bucket=S3_BUCKET).get('Contents')
     image_filenames = [item['Key'] for item in response]
     urls = []
-    for filename in image_filenames:
-        url = s3.generate_presigned_url(ClientMethod="get_object",
-                                        Params={'Bucket': S3_BUCKET,
-                                                'Key': filename},
-                                        ExpiresIn=60)
+    if image_filenames:
+        for filename in image_filenames:
+            url = s3.generate_presigned_url(ClientMethod="get_object",
+                                            Params={'Bucket': S3_BUCKET,
+                                                    'Key': filename},
+                                            ExpiresIn=60)
         urls.append(url)
-    print(urls)
     return render(request, 'image_view/image_view.html', {'urls': urls})
 
