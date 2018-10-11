05e1ac093edfa62b42688732efaf33a6b2853f1e
benghaun <ang.benghaun@gmail.com>
2018-10-11 14:20:20 +0300

    masked django secret

--- venue/settings.py
+++ venue/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3y6-%^zjog@#&(rt-=tma+*k5z@d)(xq@xv#t*6h-cnn5_1m=+'
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 SECURE_SSL_REDIRECT = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
