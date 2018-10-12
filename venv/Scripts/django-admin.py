0af76fbed1204dd909e5486fee67d5af7ba4bf7f
benghaun <ang.benghaun@gmail.com>
2018-10-12 15:20:56 +0300

    updated gitignore

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\OneDrive - Singapore University of Technology and Design\Aalto\Aalto_WWWServices\Venue\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
