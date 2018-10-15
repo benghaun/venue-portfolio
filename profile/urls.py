5a61ca5b86c2562e888498a11c80f7a3d15d52b4
Kenjyi <kenjyi_lim@mymail.sutd.edu.sg>
2018-10-15 18:19:13 +0300

    Added responsive gallery viewer

--- profile/urls.py
+++ profile/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     url(r'^$', views.profile),
     url(r'^(?P<category>.*)/img-view$', views.image),
     url(r'^(?P<category>.*)/img-view/$', views.image),
-    url(r'^(?P<category>.*)$', views.category),
     url(r'^(?P<category>.*)/$', views.category, name='category'),
 ]
