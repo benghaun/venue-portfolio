5e99dcc733f456dd871530d4b18108a874b51978
benghaun <ang.benghaun@gmail.com>
2018-10-16 21:09:00 +0300

    download compressed images for thumbnails

--- profile/views.py
+++ profile/views.py
@@ -14,10 +14,11 @@ def category(request, category):
     urls = {}
     for image in images:
         image_id = str(image.id)
+        key = image_id + "." + image.ext
         s3 = boto3.client('s3', region_name='eu-west-3')
         url = s3.generate_presigned_url(ClientMethod="get_object",
                                         Params={'Bucket': S3_BUCKET,
-                                                'Key': image_id},
+                                                'Key': "resized/" + key},
                                         ExpiresIn=86400)
         urls[url] = {'key': image_id}
 
@@ -32,6 +33,7 @@ def category(request, category):
 def image(request, category):
     s3 = boto3.client('s3', region_name='eu-west-3')
     urls = {}
+    thumbnail_urls = {}
     selected = request.GET.get("selected")
     images = Image.objects.filter(tags__contains=[category.lower()])
     image_ids = []
@@ -41,9 +43,16 @@ def image(request, category):
         image_ids.append(image_id)
         url = s3.generate_presigned_url(ClientMethod="get_object",
                                         Params={'Bucket': S3_BUCKET,
-                                                'Key': image_id},
+                                                'Key': image_id + "." + image.ext},
                                         ExpiresIn=86400)
         urls[url] = {"key": image_id, "idx": i}
+        thumbnail_url = s3.generate_presigned_url(ClientMethod="get_object",
+                                                  Params={'Bucket': S3_BUCKET,
+                                                          'Key': 'resized/' + image_id + "." + image.ext},
+                                                  ExpiresIn=86400)
+        thumbnail_urls[thumbnail_url] = {"key": image_id, "idx": i}
+
     if selected not in image_ids:
         selected = None
-    return render(request, 'profile/img-view.html', {'urls': urls, 'selected': selected})
+
+    return render(request, 'profile/img-view.html', {'urls': urls, 'selected': selected, 'thumbnail_urls': thumbnail_urls})
