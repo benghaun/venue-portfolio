ae5d73e15d112e4a9beefe7aefd3c8316ba75c16
Kenjyi <kenjyi_lim@mymail.sutd.edu.sg>
2018-11-19 14:49:27 +0200

    Edit input added, wip

--- templates/js/category.js
+++ templates/js/category.js
@@ -1,1 +1,10 @@
 var description = document.getElementById('cat-intro');
+
+function editDescription(currentDesc){
+	var new_desc = document.createElement('input')
+	new_desc.type = "text";
+	new_desc.value = currentDesc;
+	new_desc.className = "desc-input";
+	description.appendChild(new_desc);
+	new_desc.select();
+}
