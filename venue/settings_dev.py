4d7e91c5e398a1a109e99fac2419e9e57c248226
benghaun <ang.benghaun@gmail.com>
2018-11-23 17:03:35 +0200

    404

--- venue/settings_dev.py
+++ venue/settings_dev.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '3y6-%^zjog@#&(rt-=tma+*k5z@d)(xq@xv#t*6h-cnn5_1m=+'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
