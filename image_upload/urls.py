d3f09a1e87192039a5c3e6d66d55889a47fb8b49
benghaun <ang.benghaun@gmail.com>
2018-11-23 17:04:12 +0200

    removed old pages

--- image_upload/urls.py
+++ image_upload/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_view),
     path('sign_s3/', views.sign_s3),
     path('add_tag/', views.add_tag),
     path('profilepic/', views.upload_profile_pic),
--- image_view/urls.py
+++ image_view/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_images),
-    path('delete_image/', views.delete_image)
 ]
